90fa2e912946a8f328d493abd5f210a17dd23ffa
MyPOpia <guacamole0014@gmail.com>
2025-07-15 01:28:36 +0200

    client.py

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
-# client.py
 
+import os
 import discord
 from discord.ext import commands
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-TOKEN = "MTM5NDMzMzgwNzk0NzAyNjQzMg.GY2Cec.Uv-gauTs_RjO8cPFd9fuynGohtWIEQVagbPrHw"
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
+                     
 @bot.event
-async def on_ready():
-    print(f"✅ Le bot est connecté en tant que {bot.user}")
+async
 
