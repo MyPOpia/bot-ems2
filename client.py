5858758f51a1adb5115d66ff962bc742a4c16f7e
MyPOpia <guacamole0014@gmail.com>
2025-07-15 01:47:12 +0200

    client.py

--- client.py
+++ client.py
@@ -1,13 +1,20 @@
-
 import os
 import discord
 from discord.ext import commands
-TOKEN = os.getenv("DISCORD_TOKEN")
+from keep_alive import keep_alive
 
 intents = discord.Intents.all()
-
 bot = commands.Bot(command_prefix="!", intents=intents)
-                     
-@bot.event
-async
 
+
+for filename in os.listdir("./events"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"events.{filename[:-3]}")
+
+keep_alive()
+
+token = os.getenv("DISCORD_TOKEN")
+if not token:
+    print("Erreur : La variable d'environnement DISCORD_TOKEN n'est pas définie.")
+else:
+    bot.run(token)
