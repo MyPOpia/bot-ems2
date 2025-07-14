bb9c47cdcaa244a0494de791129bb464c77f88bf
MyPOpia <guacamole0014@gmail.com>
2025-07-15 01:40:17 +0200

    start.py

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
+import os  
+from keep_alive import keep_alive
+
 for filename in os.listdir("./events"):
     if filename.endswith(".py"):
         bot.load_extension(f"events.{filename[:-3]}")
-        
-from keep_alive import keep_alive
 
-keep_alive()  
+keep_alive()
