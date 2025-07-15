abd94515874d9b446fff730bf6a56151a48203c2
MyPOpia <guacamole0014@gmail.com>
2025-07-15 15:14:19 +0200

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from keep_alive import keep_alive
 import asyncio
 from db import init_storage
+from cogs.panel import PanelView  # nécessaire pour add_view()
 
 load_dotenv()
 
@@ -15,6 +16,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
+    if not hasattr(bot, "view_added"):
+        bot.add_view(PanelView())  # ✅ Une seule fois
+        bot.view_added = True
     print(f"✅ Connecté en tant que {bot.user}")
 
 async def main():
