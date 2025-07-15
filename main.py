b4633e84848fd06eb32ea8ad21b425f0a5ef599a
MyPOpia <guacamole0014@gmail.com>
2025-07-15 12:28:50 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import discord
+from discord.ext import commands
+from keep_alive import keep_alive
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+initial_extensions = [
+    "cogs.events",
+    "cogs.commands"
+]
+
+for ext in initial_extensions:
+    try:
+        bot.load_extension(ext)
+        print(f"✅ Chargé : {ext}")
+    except Exception as e:
+        print(f"❌ Erreur de chargement {ext} : {e}")
+
+keep_alive()
+
+token = os.getenv("DISCORD_TOKEN")
+
+if not token:
+    print("❌ Erreur : La variable d'environnement DISCORD_TOKEN n'est pas définie.")
+else:
+    bot.run(token)
+
